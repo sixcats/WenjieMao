eae28bbb40ffda4b3f14cac7e05dbe01985f4499
sixcats <mwj19990518@gmail.com>
2024-08-16 18:08:38 +1000

    week3&4 efolio

---
+++ wenjiemao-library/src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import JSON from "./components/JSON.vue"
+//import JSONSolution from "./components/JSONSolution.vue"
+import Form from "./components/Form.vue"
+</script>
+
+<template>
+  <!-- <JSON /> -->
+  <!-- <JSONSolution />-->
+   <Form/>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 768px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+  .form-row {
+        flex-direction: column;
+        align-items: stretch;
+    }
+
+  .form-row input {
+        margin-bottom: 10px;
+        margin-right: 0;
+    }
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
