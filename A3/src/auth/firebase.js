5b15a0d310881276078e04515313916de9c60ee7
sixcats <mwj19990518@gmail.com>
2024-10-14 00:32:11 +1100

    update fireauth api information

--- A3/src/auth/firebase.js
+++ A3/src/auth/firebase.js
@@ -3,17 +3,12 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCRzdTcVbnFcJoWJk6mxCRoTSXfo-sIPuI",
-    authDomain: "test-1f883.firebaseapp.com",
-    projectId: "test-1f883",
-    storageBucket: "test-1f883.appspot.com",
-    messagingSenderId: "791952997929",
-    appId: "1:791952997929:web:e958073c7a812651812638",
-    measurementId: "G-7FQPKNTXK3"
-
-
-  
-
+  apiKey: "AIzaSyBJgg2A0AaFGaKg11I9pYozmKypd8HFxbE",
+  authDomain: "week7-wenjie.firebaseapp.com",
+  projectId: "week7-wenjie",
+  storageBucket: "week7-wenjie.appspot.com",
+  messagingSenderId: "570249966700",
+  appId: "1:570249966700:web:b0606afb99ae20be289449"
 };
 
 const app = initializeApp(firebaseConfig);
